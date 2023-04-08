efc1dd515068bdd44cf64b86c6c53850a69daf67
CPranaviReddy <115911247+CPranaviReddy@users.noreply.github.com>
2023-04-08 18:59:07 +0530

    Update API.js

--- app/src/utils/API.js
+++ app/src/utils/API.js
@@ -11,24 +11,24 @@ export default {
 
   // imported from App.js (check for a valid token)
   checkValidToken: function (token) {
-    return axios.post('/isValidToken', null, {
+    return client.post('/isValidToken', null, {
       headers: { 'x-auth-token': token },
     });
   },
 
   // imported from App js (after checking the token is valid,fetching the user information)
   authenticateUser: function (token) {
-    return axios.post('/', { headers: { 'x-auth-token': token } });
+    return client.post('/', { headers: { 'x-auth-token': token } });
   },
 
   // imported login page(End Point to pass the user entered information to backend and to check the login validations)
   loginRes: function (loginUser) {
-    return axios.post('/api/login', loginUser);
+    return client.post('/api/login', loginUser);
   },
 
   // imported from Signup page(End Point to pass the user signed up info to backend and store it)
   SignUpRes: function (newUser) {
-    return axios.post('/api/signup', newUser);
+    return client.post('/api/signup', newUser);
   },
 
   
@@ -36,24 +36,24 @@ export default {
   
   //  check Journal entry for the the particular entry date
   checkAJournalEntry: function (entryDate, userId) {
-    return axios.get('/api/notes/entrydate/' + entryDate, {
+    return client.get('/api/notes/entrydate/' + entryDate, {
       params: { userId },
     });
   },
 
   // Create journal entry
   createJournalEntry: function (journalEntry) {
-    return axios.post('/api/notes', journalEntry);
+    return client.post('/api/notes', journalEntry);
   },
 
   // Get one entry by noteId and userId
   getOneJournalEntry: function (noteId, userId) {
-    return axios.get('/api/notes/' + noteId, { params: { userId } });
+    return client.get('/api/notes/' + noteId, { params: { userId } });
   },
 
   // Update one entry by noteId, userId
   updateOneJournalEntry: function (noteId, userId, updatedEntry) {
-    return axios.put('/api/notes/' + noteId, updatedEntry, {
+    return client.put('/api/notes/' + noteId, updatedEntry, {
       params: { userId },
     });
   },
@@ -61,13 +61,13 @@ export default {
 // check Journal entries based on search results for particular month
   checkASearchJournalEntry: function (month,year, userId) {
     console.log(' user id : ' + userId + "month :" +month +"year :" + year);
-    return axios.get('/api/notes' ,{
+    return client.get('/api/notes' ,{
       params: { month,year,userId }
     });
   },
 
   // Delete one entry by noteId, userId
   removeOneJournalEntry: function (noteId, userId) {
-    return axios.delete('/api/notes/' + noteId, { params: { userId } });
+    return client.delete('/api/notes/' + noteId, { params: { userId } });
   },
 };
